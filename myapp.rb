c9d09356c4968054f2d9582009e65ddb8c9eace4
ivanpopovic <i.popovic@gmx.at>
2017-03-17 12:31:23 +0100

    initial commit

---
+++ myapp.rb
@@ -0,0 +1,13 @@
+# myapp.rb
+require 'sinatra'
+require_relative 'phone_number'
+
+get '/' do
+  slim :index
+end
+
+post '/' do
+  @number = params[:number]
+  @normalized = PhoneNumber.new(@number).normalized
+  slim :index
+end
